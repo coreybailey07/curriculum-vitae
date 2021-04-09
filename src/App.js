beafdf320cb7c07e2ffc2e42e277db1df760d081
Corey Bailey <public@coreybailey.net>
2021-04-09 17:19:06 -0400

    Add Portfolio related components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { Header } from './component/header/header.component';
 import { About } from './component/about/about.component';
 import { Resume } from './component/resume/resume.component';
+import { Portfolio } from './component/portfolio/portfolio.component';
 import { Contact } from './component/contact/contact.component';
 import { Testimonial } from './component/testimonial/testimonial.component';
 import { CallToAction } from './component/cta/call-to-action.component';
@@ -18,6 +19,7 @@ function App() {
       <main>
         <About />
         <Resume />
+        <Portfolio />
         <CallToAction />
         <Testimonial />
         <Contact />
--- src/component/modal/modal.component.jsx
+++ src/component/modal/modal.component.jsx
@@ -1,85 +1,9 @@
 import React from 'react';
 
-import './portfolio.style.scss';
+import './modal.style.scss';
 
-export const Portfolio = () => (
-  <section id='portfolio' className='s-portfolio target-section'>
-
-    <div className='row s-portfolio__header'>
-      <div className='column large-12'>
-        <h3>A Few Of My Latest Creations</h3>
-      </div>
-    </div>
-
-    <div className='row collapse block-large-1-4 block-medium-1-3 block-tab-1-2 block-500-stack folio-list'>
-
-      <div className='column folio-item'>
-        <a href='#modal-01' className='folio-item__thumb'>
-          <img src={'images/portfolio/droplet.jpg'}
-            srcSet={`images/portfolio/droplet.jpg 1x, 
-                           images/portfolio/droplet@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-02' className='folio-item__thumb'>
-          <img src={'images/portfolio/lamp.jpg'}
-            srcSet={`images/portfolio/lamp.jpg 1x, 
-                           images/portfolio/lamp@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-03' className='folio-item__thumb'>
-          <img src={'images/portfolio/minimalismo.jpg'}
-            srcSet={`images/portfolio/minimalismo.jpg 1x, 
-                           images/portfolio/minimalismo@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-04' className='folio-item__thumb'>
-          <img src={'images/portfolio/shutterbug.jpg'}
-            srcSet={`images/portfolio/shutterbug.jpg 1x, 
-                           images/portfolio/shutterbug@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-05' className='folio-item__thumb'>
-          <img src={'images/portfolio/fuji.jpg'}
-            srcSet={`images/portfolio/fuji.jpg 1x, 
-                           images/portfolio/fuji@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-06' className='folio-item__thumb'>
-          <img src={'images/portfolio/skaterboy.jpg'}
-            srcSet={`images/portfolio/skaterboy.jpg 1x, 
-                           images/portfolio/skaterboy@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-07' className='folio-item__thumb'>
-          <img src={'images/portfolio/rucksack.jpg'}
-            srcSet={`images/portfolio/rucksack.jpg 1x, 
-                           images/portfolio/rucksack@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-      <div className='column folio-item'>
-        <a href='#modal-08' className='folio-item__thumb'>
-          <img src={'images/portfolio/sanddunes.jpg'}
-            srcSet={`images/portfolio/sanddunes.jpg 1x, 
-                           images/portfolio/sanddunes@2x.jpg 2x`} alt='' />
-        </a>
-      </div>
-
-    </div>
-
-
+export const Modal = () => (
+  <div>
     <div id='modal-01' hidden>
       <div className='modal-popup'>
         <img src={'images/portfolio/gallery/g-droplet.jpg'} alt='' />
@@ -211,7 +135,5 @@ export const Portfolio = () => (
         <a href='https://www.behance.net/' className='modal-popup__details'>Project link</a>
       </div>
     </div>
-
-  </section>
-
+  </div>
 )
