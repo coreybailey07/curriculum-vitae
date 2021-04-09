b803585188dec3bfa21bb0cb9b07b2801635a007
Corey Bailey <public@coreybailey.net>
2021-04-09 10:17:52 -0400

    Add about component and main content area element

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 
 import { Nav } from './component/nav/nav.component';
 import { Hero } from './component/hero/hero.component';
+import { About } from './component/about/about.component';
 import { Footer } from './component/footer/footer.component';
 
 function App() {
   return (
     <div className='App'>
       <Nav />
-      <Hero />
+      <main>
+        <Hero />
+        <About />
+      </main>
       <Footer />
     </div>
   );
