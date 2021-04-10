0ce0a2d23e6bf1f765898bfca72a967bd4be553e
Corey Bailey <public@coreybailey.net>
2021-04-10 12:34:40 -0400

    Fix content shift due to reactjs compression

--- src/component/about/about.component.jsx
+++ src/component/about/about.component.jsx
@@ -40,11 +40,8 @@ export const About = () => (
           </div>
           <div className='column w-1000-stack'>
             <a href='#0' className='btn btn--download'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' style={{fill:'rgba(0, 0, 0, 1)',transform:'',msFilter:''}}>
-                <path d='M12 16L16 11 13 11 13 4 11 4 11 11 8 11z'></path>
-                <path d='M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z'></path>
-              </svg>
-              Download CV
+              <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' style={{'fill':'rgba(0, 0, 0, 1)','transform':'','msFilter':''}}><path d='M12 16L16 11 13 11 13 4 11 4 11 11 8 11z'></path><path d='M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z'></path></svg>
+              {' '}Download CV
             </a>
           </div>
         </div>
