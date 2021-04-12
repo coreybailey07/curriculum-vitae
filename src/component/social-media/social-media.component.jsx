27b1ec081c708b3888dab3f5e11947a18c6cb57c
Corey Bailey <public@coreybailey.net>
2021-04-12 16:49:16 -0400

    Add map function and const array

--- src/component/social-media/social-media.component.jsx
+++ src/component/social-media/social-media.component.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 import './social-media.style.scss';
 
-const SocialMedia = () => (
-  <div className='s-hero__content-social'>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-facebook-square' /></a>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-twitter' /></a>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-instagram' /></a>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-dribbble' /></a>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-behance' /></a>
-    <a aria-hidden='true' onClick={(e) => {e.preventDefault()}} href='#/'><i className='fab fa-linkedin' /></a>
-  </div>
-)
+const SocialMedia = () => {
 
-export default SocialMedia;
+  const socialMediaData = [
+    { id: 1, selector: 'fab fa-facebook-square' },
+    { id: 2, selector: 'fab fa-twitter' },
+    { id: 3, selector: 'fab fa-instagram' },
+    { id: 4, selector: 'fab fa-dribbble' },
+    { id: 5, selector: 'fab fa-behance' },
+    { id: 6, selector: 'fab fa-linkedin' }
+  ];
+
+  return (
+    <div className='s-hero__content-social'>
+      {socialMediaData.map((sm) => (
+        <a aria-hidden='true' onClick={(e) => { e.preventDefault() }} href='#/'><i className={sm.selector} /></a>
+      ))}
+    </div>
+  )
+}
+
+export default SocialMedia
