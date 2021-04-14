da36733657bc9a428da4e7061ec766eafadddb25
Corey Bailey <public@coreybailey.net>
2021-04-14 14:20:34 -0400

    Change resume component
    * Remove ResumePrototype component
    * Reorder imports

--- src/component/resume/resume.component.jsx
+++ src/component/resume/resume.component.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import './resume.style.scss';
 
-import ResumePrototype from './resume-prototype.component';
-import Skills from '../skills/skills.component';
-import Education from '../education/education.component';
 import Career from '../career/career.component';
+import Education from '../education/education.component';
+import Skills from '../skills/skills.component';
 
 const Resume = () => (
   <section id='resume' className='s-resume target-section'>
-
-    <ResumePrototype />
-    
     <Career />
     <Education />
     <Skills />
