8b67c81eb150ea726fe37c31c4b267bdabf34279
Corey Bailey <public@coreybailey.net>
2021-04-14 14:36:29 -0400

    Update social-media.component.jsx
    * Add json properties.
    * Change map() parent.

--- src/component/social-media/social-media.component.jsx
+++ src/component/social-media/social-media.component.jsx
@@ -4,22 +4,23 @@ import './social-media.style.scss';
 
 const SocialMedia = () => {
 
-  const socialMediaData = [
-    { id: 0, selector: 'fab fa-facebook-square' },
-    { id: 1, selector: 'fab fa-twitter' },
-    { id: 2, selector: 'fab fa-instagram' },
-    { id: 3, selector: 'fab fa-dribbble' },
-    { id: 4, selector: 'fab fa-behance' },
-    { id: 5, selector: 'fab fa-linkedin' }
+  const data = [
+    { id:0, name:'Facebook', url:'https://www.facebook.com/', selector:'fab fa-facebook-square' },
+    { id:1, name:'Twitter', url:'https://twitter.com/?lang=en', selector:'fab fa-twitter' },
+    { id:2, name:'Instagram', url:'https://www.instagram.com/', selector:'fab fa-instagram' },
+    { id:3, name:'Dribbble', url:'https://dribbble.com/', selector:'fab fa-dribbble' },
+    { id:4, name:'Behance', url:'https://www.behance.net/', selector:'fab fa-behance' },
+    { id:5, name:'LinkedIn', url:'https://www.linkedin.com/', selector:'fab fa-linkedin' }
   ];
 
   return (
     <div className='s-hero__content-social'>
-      {socialMediaData.map((sm) => (
+      {data.map((sm) => (
         <a key={sm.id} aria-hidden='true' onClick={(e) => { e.preventDefault() }} href='#/'><i className={sm.selector} /></a>
       ))}
     </div>
   )
+
 }
 
 export default SocialMedia;
