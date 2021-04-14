e7fa87a65e5c1e44cdf62b4c175e4f6cd2f946cb
Corey Bailey <public@coreybailey.net>
2021-04-14 14:21:24 -0400

    Update resume-prototype.component.jsx
    * Final Changes before removal.

--- src/component/resume/resume-prototype.component.jsx
+++ src/component/resume/resume-prototype.component.jsx
@@ -9,9 +9,9 @@ const ResumePrototype = () => {
       section: "Career",
       position: [{
         company: "Dropbox",
-        title: "Lead Engineer",
-        from: "January 1999",
-        to: "September 2021",
+        title: "Product Designer",
+        from: "August 2019",
+        to: "Present",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet. Vitae aut atque fuga dolorem. Vel voluptatibus fugiat nam. Impedit aperiam nesciunt facilis! Porro architecto dicta inventore tempora ratione quia odio.",
         listItems: [
           {
@@ -25,52 +25,70 @@ const ResumePrototype = () => {
           }
         ]
       }, {
-        company: "Super Electronics",
-        title: "Production Designer",
-        from: "January 2009",
-        to: "September 2014",
+        company: "Facebook",
+        title: "UI/UX Designer",
+        from: "August 2016",
+        to: "July 2019",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet. Vitae aut atque fuga dolorem. Vel voluptatibus fugiat nam. Impedit aperiam nesciunt facilis! Porro architecto dicta inventore tempora ratione quia odio.",
 
       }]
     },
     {
-      section: "Jobs",
+      section: "Education",
       position: [{
-        company: "Google",
-        title: "Production Lead Engineer",
-        from: "January 2002",
-        to: "September 2015",
+        company: "University of Life",
+        title: "Master in Graphic Design",
+        from: "August 2019",
+        to: "Present",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet. Vitae aut atque fuga dolorem. Vel voluptatibus fugiat nam. Impedit aperiam nesciunt facilis! Porro architecto dicta inventore tempora ratione quia odio.",
+        listItems: [
+          {
+            item: "Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet."
+          },
+          {
+            item: "Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet."
+          },
+          {
+            item: "Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet."
+          }
+        ]
+      }, {
+        company: "School of Cool Designers",
+        title: "B.A. Degree in Graphic Design  ",
+        from: "August 2016",
+        to: "July 2019",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore vero quidem nobis maxime dolorem aliquam quisquam eum ipsum amet. Vitae aut atque fuga dolorem. Vel voluptatibus fugiat nam. Impedit aperiam nesciunt facilis! Porro architecto dicta inventore tempora ratione quia odio.",
+
       }]
     },
   ];
 
   return (
     resume.map((res, i) => (
-    <div className='row s-resume__section'>
-          <div className='column large-3 tab-12'>
-            <h3 className='section-header-allcaps'>{res.section}</h3>
-          </div>
-          <div className='column large-9 tab-12'>
-            {res.position.map((pos, j) => (
-              <div key={j+1} className='resume-block'>
+      <div className='row s-resume__section'>
+        <div className='column large-3 tab-12'>
+          <h3 className='section-header-allcaps'>{res.section}</h3>
+        </div>
+        <div className='column large-9 tab-12'>
+          {res.position.map((pos, j) => (
+            <div key={j + 1} className='resume-block'>
 
-                <div className='resume-block__header'>
-                  <h4 className='h3'>{pos.company}</h4>
-                  <p className='resume-block__header-meta'>
-                    <span>{pos.title}</span>
-                    <span className='resume-block__header-date'>
-                      {pos.from} - {pos.to}
-                    </span>
-                  </p>
-                </div>
-
-                <p>{pos.description}</p>
-
+              <div className='resume-block__header'>
+                <h4 className='h3'>{pos.company}</h4>
+                <p className='resume-block__header-meta'>
+                  <span>{pos.title}</span>
+                  <span className='resume-block__header-date'>
+                    {pos.from} - {pos.to}
+                  </span>
+                </p>
               </div>
-            ))}
-          </div>
-    </div>
+
+              <p>{pos.description}</p>
+
+            </div>
+          ))}
+        </div>
+      </div>
     ))
   )
 }
