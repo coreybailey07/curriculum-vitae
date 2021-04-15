21959068ccb16c0928fbb4a3d59084e6bb58c8ec
Corey Bailey <public@coreybailey.net>
2021-04-15 16:47:50 -0400

    Update Skill-bars.component * Add json and map() syntax

--- src/component/skill-bars/skill-bars.component.jsx
+++ src/component/skill-bars/skill-bars.component.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 
 import './skill-bars.style.scss';
 
-const SkillBars = () => (
-  <ul className='skill-bars-fat'>
-    <li>
-      <div className='progress percent85'></div>
-      <strong>HTML</strong>
-    </li>
-    <li>
-      <div className='progress percent85'></div>
-      <strong>CSS</strong>
-    </li>
-    <li>
-      <div className='progress percent65'></div>
-      <strong>Javascript</strong>
-    </li>
-    <li>
-      <div className='progress percent90'></div>
-      <strong>Figma</strong>
-    </li>
-    <li>
-      <div className='progress percent75'></div>
-      <strong>Illustrator</strong>
-    </li>
-    <li>
-      <div className='progress percent60'></div>
-      <strong>Photoshop</strong>
-    </li>
-  </ul>
-)
+const SkillBars = () => {
+
+  const data = [
+    { name: "HTML", compentancy: 100 },
+    { name: "CSS", compentancy: 100 },
+    { name: "JavaScript", compentancy: 90 },
+    { name: "Bootstrap", compentancy: 80 },
+    { name: "jQuery", compentancy: 85 },
+    { name: "React", compentancy: 75 },
+    { name: "Angular", compentancy: 65 },
+    { name: "Photoshop", compentancy: 80 },
+    { name: "Figma", compentancy: 50 },
+    { name: "MongoDB", compentancy: 45 },
+    { name: "SQL", compentancy: 40 }
+  ];
+
+  return (
+    <ul className='skill-bars-fat'>
+      {data.map((skl, i) => (
+        <li key={i} className="progress-bar">
+          <div style={{ width: skl.compentancy + '%' }}><span className="skill">{skl.name}</span><span className="compentancy">{skl.compentancy + '%'}</span></div>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default SkillBars;
